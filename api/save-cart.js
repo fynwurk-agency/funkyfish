fa0604da19ec2392a3efe914fff68cce0d87b40f
Fynwurk <fynwurk@gmail.com>
2026-07-27 00:16:31 +0530

    Save Cart Updated 2024-01 to 2025-10

--- api/save-cart.js
+++ api/save-cart.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     const ADMIN_API_TOKEN = process.env.SHOPIFY_ADMIN_TOKEN;
 
     const response = await fetch(
-      `https://${SHOP_DOMAIN}/admin/api/2024-01/customers/${customerId}/metafields.json`,
+      `https://${SHOP_DOMAIN}/admin/api/2025-10/customers/${customerId}/metafields.json`,
       {
         method: "POST",
         headers: {
